80c62a77afcbf5e4c96a0f6bbf7182aaf8c01f94
Nathan VandenHoek <vandenng@eos10.cis.gvsu.edu>
2018-02-23 10:34:34 -0500

    Final Changes to iToP() & balancedSymbols()

---
+++ include/functions.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+#include <iostream>
+
+bool balancedSymbols(std::string input);
+std::string iToP(std::string input);
+bool prec( char ch, char ch2 );
+
+#endif
+
---
+++ src/functions.cpp
@@ -0,0 +1,106 @@
+//Author Nathan Vanden Hoek
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+#include "functions.h"
+
+/*******************************************************************************************
+ * This method will return ture if all symbols (,{,[, have a corrisponding ),},], and false
+ * if they do not.
+ *******************************************************************************************/
+bool balancedSymbols(std::string input){
+	std::stack <char, std::deque<char> > symbols;
+	
+	for( auto it = input.begin(); it != input.end(); ++it ){
+		if( *it == '(' || *it == '{' || *it == '[' )
+			symbols.push(*it);
+		else if( *it == ')' && symbols.top() == '(')
+			symbols.pop();
+		else if( *it == ']' && symbols.top() == '[' )
+			symbols.pop();
+		else if ( *it == '}' && symbols.top() == '{' )
+			symbols.pop();
+	}
+	
+	return symbols.size();
+}
+
+/*******************************************************************************************
+ *iToP stands for Infix to Postfix. This method withh take an input string, and convert it
+ *to postfix as the returned string. 
+ *******************************************************************************************/
+std::string iToP( std::string input ){
+	std::string postFix;
+	std::stack <char, std::deque<char> > operators;
+
+	if( balancedSymbols( input ) ){
+		std::cout << "ERROR: Input error, please try again." << std::endl;
+		return postFix;
+	}
+
+	for( auto it = input.begin( ); it != input.end(); ++it ){
+		if( *it == '+' || *it == '-' || *it == '*' || *it == '/' || *it == '(' || *it == ')' || *it == '^' ){	
+				
+			while( operators.size() > 0 && prec(operators.top(), *it) ){
+				postFix.push_back( operators.top() );
+				operators.pop();	
+			}
+
+			if(operators.size() == 0 || *it != ')'){
+				operators.push( *it );
+			}else{
+				operators.pop();	
+			}
+
+		}else if( *it != ' ' ){
+			postFix.push_back( *it );
+		}	
+	}
+	
+	while ( operators.size() > 0 ){ 
+		postFix.push_back( operators.top() );
+		operators.pop();
+	}
+	
+	return postFix;
+}
+
+/*******************************************************************************************
+ * This is a helper method to state the precedance of the operatores used in iToP(). 
+ *******************************************************************************************/
+bool prec( char ch, char ch2 ){
+	if( ch == '(' )
+		return false;
+	else if( ch2 == '(' )
+		return false;
+	else if( ch2 == ')' )
+		return true;
+	else if( ch == '*' || ch == '/' )
+		return true;
+	else if ( ch2 == '+' || ch2 == '-' )
+		return true;
+	else
+		return false;
+}
+
+/*******************************************************************************************
+ *Used to test the functionality of iToP() & Balanced Symbols! 
+ *******************************************************************************************/
+int main( int argc, char** argv ){
+	
+	std::cout << iToP("A + B * C + D") << std::endl;
+	std::cout << iToP("(A + B) * (C + D)") << std::endl;
+	std::cout << iToP("A * B + C * D") << std::endl;
+	std::cout << iToP("A + B + C + D") << std::endl;
+	std::cout << iToP("A * B + C / D") << std::endl;
+	std::cout << iToP("A * (B + C) / D") << std::endl;
+	std::cout << iToP("A * (B + C / D)") << std::endl;
+	std::cout << iToP("A + B * C - D / E * F") << std::endl;
+	std::cout << iToP("(A + B * C - D) / (E * F)") << std::endl;
+	std::cout << iToP("((A + B) * (C - D ) + E) / (F + G)") << std::endl;
+	std::cout << iToP(" ((((a + b) + c) - d) + { g }") << std::endl; 
+	
+	return EXIT_SUCCESS;
+}
